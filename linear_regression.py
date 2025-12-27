252782165641c67178291b393c3704abff9b5808
Last9541 <147716467+Last9541@users.noreply.github.com>
2025-12-27 19:51:15 +0100

    Ispravka log transformacije

--- linear_regression.py
+++ linear_regression.py
@@ -20,7 +20,7 @@ def run_linear_regression():
     X = X[mask]
     y = y[mask]
 
-    y = np.log(y)
+    y = np.log1p(y)
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
